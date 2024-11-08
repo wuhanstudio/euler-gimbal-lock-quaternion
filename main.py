76313758bbf1a7e04e90e2602b097fae7d35f0a4
wuhanstudio <wuhanstudios@gmail.com>
2024-11-08 20:33:36 +0000

    Load Object

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+import sys, pygame
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+# IMPORT OBJECT LOADER
+from objloader import *
+
 verticies = (
     (1, -1, -1),
     (1, 1, -1),
@@ -52,6 +61,25 @@ def main():
 
     pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.OPENGL | pygame.RESIZABLE)
 
+    glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+
+    # LOAD OBJECT AFTER PYGAME INIT
+    obj = OBJ("airplane/airplane.obj", swapyz=True)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    width, height = size
+    gluPerspective(90.0, width/float(height), 1, 100.0)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_MODELVIEW)
+
     imgui.create_context()
     impl = PygameRenderer()
 
@@ -62,11 +90,35 @@ def main():
 
     glTranslatef(0.0,0.0, -5)
 
+    rx, ry = (0,0)
+    tx, ty = (0,0)
+    zpos = 5
+    rotate = move = False
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if event.button == 4: zpos = max(1, zpos-1)
+                elif event.button == 5: zpos += 1
+                elif event.button == 1: rotate = True
+                elif event.button == 3: move = True
+            elif event.type == MOUSEBUTTONUP:
+                if event.button == 1: rotate = False
+                elif event.button == 3: move = False
+            elif event.type == MOUSEMOTION:
+                i, j = event.rel
+                if rotate:
+                    rx += i
+                    ry += j
+                if move:
+                    tx += i
+                    ty -= j
             impl.process_event(event)
+
         impl.process_inputs()
 
         imgui.new_frame()
@@ -102,7 +154,15 @@ def main():
         glRotatef(1, 3, 1, 1)
         impl.render(imgui.get_draw_data())
 
-        Cube()
+        # Cube()
+
+        # RENDER OBJECT
+        glTranslate(tx/20., ty/20., - zpos)
+        glRotate(ry, 1, 0, 0)
+        glRotate(rx, 0, 1, 0)
+        glCallList(obj.gl_list)
+
+        glLoadIdentity()
 
         pygame.time.wait(10)
         pygame.display.flip()
